c9db71804e94769834f241acbc78c6ae53e79ab2
Tyler Le <tylerle99@yahoo.com>
2022-10-31 19:39:09 -0700

    fetch data prototype

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+button = document.getElementById("get-data");
+button.addEventListener("click", (event) => {
+  handle_data();
+});
 
+function handle_data() {
+  fetch("data/metasys_example_data.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.items);
+      appendData(data.items);
+    })
+    .catch(function (err) {
+      console.log("error: " + err);
+    });
+  function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      console.log(data[i]);
+      div.innerHTML = `Milliwatts: ${data[i].value.value}, Date: ${data[i].timestamp}, Reliable: ${data[i].isReliable}`;
+      mainContainer.appendChild(div);
+    }
+  }
+}
